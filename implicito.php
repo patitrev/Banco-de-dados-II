522099313db62ef729e07b75503949d1cf3172e7
Patrícia Trevisan <patitrev@hotmail.com>
2021-12-15 17:12:50 -0300

    Add files via upload

---
+++ implicito.php
@@ -0,0 +1,43 @@
+<?php
+$connString = 'host = localhost dbname= teste user=postgres password=senha';
+
+ 
+try{ 
+
+    $db = pg_connect($connString);
+    echo "Connected to Postgres with PDO";
+
+}catch(PDOExeption $e){
+    echo "Conection fail";
+    die($e->getMessage());
+}
+
+$initialTime = 0;
+$finalTime = 0;
+$erro = 0;
+ 
+$handle = fopen("reddit_vm.csv", "r");
+
+$initialTime = microtime(true);
+
+while ($line = fgetcsv($handle,1000,",")){
+
+    $insert["title"] = mb_convert_encoding($line[0], "UTF-8");
+    $insert["score"] =  intval(mb_convert_encoding($line[1], "UTF-8"));
+    $insert["id"] = intval(mb_convert_encoding($line[2], "UTF-8"));
+    $insert["url"] = mb_convert_encoding($line[3], "UTF-8");
+    $insert["comms_num"] = mb_convert_encoding($line[4], "UTF-8");
+    $insert["created"] = mb_convert_encoding($line[5], "UTF-8");
+    $insert["body"] = mb_convert_encoding($line[6], "UTF-8");
+    $insert["timestamp"] = mb_convert_encoding($line[7], "UTF-8");
+
+    pg_insert($db, 'covid', $insert);
+}
+
+ 
+
+$finalTime = microtime(true);
+fclose($handle);
+pg_close($db);
+echo "</br> Implicito = " . strval($finalTime-$initialTime) . " sec </br>";
+?>
